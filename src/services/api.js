d774eb90b705404d1064e24c717953a2bc18dd97
Andraelja <eljapramaandra@gmail.com>
2025-10-01 16:15:06 +0700

    perbaikan

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const Api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api" 
-      : "https://backend-portofolio-eight.vercel.app",
+      : "https://backend-portofolio-eight.vercel.app/api",
 });
 
 export default Api;
